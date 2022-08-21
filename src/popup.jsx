67904dae3e5926aa6d6445799fffffd8674d4e23
lilylilyngo <13605973@student.uts.edu.au>
2022-08-21 16:50:49 +1000

    add in react, webpack and other dependences

---
+++ src/popup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+function Popup() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>Smapel </p>
+        </div>
+    );
+}
+render(<Popup/>, document.getElementById("react-target"));
